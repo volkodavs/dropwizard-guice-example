228e292d543a9f6b305f3084b3c80580a5f31423
Sergii Volkodav <sergii.volkodav@fabfitfun.com>
2020-04-20 21:45:19 +0100

    replace annotation for jsr-330

--- src/main/java/com/sergeyvolkodav/dwexample/config/ServerModule.java
+++ src/main/java/com/sergeyvolkodav/dwexample/config/ServerModule.java
@@ -2,7 +2,7 @@ package com.sergeyvolkodav.dwexample.config;
 
 import com.google.inject.Binder;
 import com.google.inject.Provides;
-import com.google.inject.Singleton;
+import javax.inject.Singleton;
 import com.hubspot.dropwizard.guicier.DropwizardAwareModule;
 import com.sergeyvolkodav.dwexample.health.ResourceHealthCheck;
 import com.sergeyvolkodav.dwexample.resources.FortuneResource;
